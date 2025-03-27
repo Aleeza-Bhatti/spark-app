1f1f5b5da7cb7d91f39be2562c20fd47fe6efdf4
Aleeza-Bhatti <aleeza.bhatti@outlook.com>
2025-03-27 15:13:42 -0700

    added logo

--- spark/app/(tabs)/index.tsx
+++ spark/app/(tabs)/index.tsx
@@ -1,21 +1,37 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 
 export default function App() {
   return (
     <LinearGradient
-      colors={['#ffafbd', '#ffc3a0']}
+      colors={['#FFEAC5', '#F9B78F']}
       style={styles.container}
-      >
-      </LinearGradient>
+    >
+      <Text style={styles.title}>Word Detective</Text>
+      <Image source={require('./assets/logo.png')} style={styles.image} />
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1, // fills the screen
-    justifyContent: 'center',
+    flex: 1,
+    justifyContent: 'flex-start',
     alignItems: 'center',
+    paddingTop: 100,
+  },
+
+  image: {
+    width: 150,
+    height: 150,
+    marginTop: 0,
+  },
+
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    color: '#000', // Black text to make it visible
   },
 });
